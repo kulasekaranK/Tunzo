0aa59f7fc3c1d30331bb90925454c980ab3acf09
Guna <itzkulasekaran@gmail.com>
2025-06-29 19:15:51 +0530

    updated

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {TunzoPlayerAPI, Player} from 'tunzo-player'; // if you want all in a namespace
+
 
 @Component({
   selector: 'app-tab1',
@@ -8,6 +10,20 @@ import { ExploreContainerComponent } from '../explore-container/explore-containe
   styleUrls: ['tab1.page.scss'],
   imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit {
+  results:any;
+  constructor() {
+   console.log(this.results);
+   
+  }
+ async ngOnInit(){
+    const api = new TunzoPlayerAPI();
+    this.results = await api.searchSongs('popular songs');
+    console.log(this.results);
+    Player.initialize(this.results, 3);
+    Player.play(this.results[7], 3);
+    Player.addToQueue(this.results[6]);
+    console.log( Player.getQueue());
+    
+  }
 }
