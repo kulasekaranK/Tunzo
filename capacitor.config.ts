1a0ff20669f446202c8a8d89061cce98c17605a4
Guna <itzkulasekaran@gmail.com>
2025-08-17 16:36:16 +0700

    Fix casing of app name in configuration files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'tunzo.app.com',
-  appName: 'tunzo',
+  appName: 'Tunzo',
   webDir: 'www'
 };
 
