2a23a3ebc0a4e7e239fabf78079ad406aadaf6e8
Guna <itzkulasekaran@gmail.com>
2025-08-27 12:58:26 +0700

    feat: update appVersion to 1.0.27 in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  appVersion: '1.0.7'
+  appVersion: '1.0.27'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  appVersion: '1.0.7'
+  appVersion: '1.0.27'
 };
 
 /*
